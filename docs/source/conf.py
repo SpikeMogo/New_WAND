9e081df9ffe353b596fb8519b488fef8e6ed280c
SpikeMogo <mogoooji@gmail.com>
2022-01-10 23:14:39 -0500

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'WAND'
+copyright = '2021, Spike'
+author = 'Spike'
 
 release = '0.1'
 version = '0.1.0'
